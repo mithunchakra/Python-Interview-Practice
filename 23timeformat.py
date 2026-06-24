5af331fe5d32b8ee39f18a3283ea7f4949f85354
Mithun H S <mithunmitu1032@gmail.com>
2026-06-24 15:52:01 +0530

    Added Time date format  problem asked in interview

---
+++ 23timeformat.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+hour = datetime.now().hour
+
+if 5 <= hour < 12:
+    print("Good Morning")
+
+elif 12 <= hour < 17:
+    print("Good Afternoon")
+
+elif 17 <= hour < 21:
+    print("Good Evening")
+
+else:
+    print("Good Night")
